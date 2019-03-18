f04f372bfbe06c229a4d7049974ee6ee91b76ef2
AERM2018 <retana.martinez.angel.eduardogmail.com>
2019-03-18 00:36:30 -0600

    primer comit

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import {Link} from 'react-router-dom';
+// import logo from "../logo.svg";
+import "../css/App.css";
+import "../css/animated.css"
+import "../css/animations.css"
+// import Croquis from './Croquis'
+
+class App extends Component {
+  render() {
+    return (
+        <div className="App">
+          <h1 className="animated fadeInUp retraso-1">Bienvenido al recorrido virtual por el CBTis #130</h1>
+          <Link to={"/Croquis"}><button type="button" className="btn btn-success animated rollIn retraso-2">Comenzar</button></Link>
+        </div>
+
+      
+      
+     
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Croquis.js
@@ -0,0 +1,22 @@
+import React,{Component} from 'react';
+import Css from "../index.css";
+class Croquis extends Component{
+    render(){
+        var mainBg={
+            backgroundImage:"none",
+            background: "black"
+        }
+        // document.getElementById('body').style.backgroundImage="none";
+        // Css.insertRule("body{backgroundImage:none}",0);
+        
+        
+        return(
+            <div className="">
+               
+               
+            </div>
+        );
+    }
+}
+
+export default Croquis;
